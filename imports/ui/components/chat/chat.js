d241b40a5ca611b3a9bbac8b97d01b0a8cc576dd
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-02 21:12:22 +0700

    tambah contoh codingan untuk chat

--- imports/ui/components/chat/chat.js
+++ imports/ui/components/chat/chat.js
@@ -3,6 +3,36 @@ import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
 import template from './chat.html';
+import { Chats } from '../../../api/chats';
+import { Users } from '../../../api/users';
+
+class Chat {
+  constructor($scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    this.helpers({
+      dataChats() {
+        return Chats.find({});
+      },
+      dataUsers() {
+        return Users.find({});
+      }
+    });
+
+    this.inputChat = {};
+    this.userChat = 'rizki mufrizal';
+  }
+
+  sendChat(chat) {
+    chat.username = 'rizki mufrizal';
+    chat.waktu = new Date();
+    Chats.insert(chat);
+    this.inputChat = {};
+  }
+
+}
 
 const name = 'chat';
 
@@ -12,6 +42,8 @@ export default angular.module(name, [
 ])
   .component(name, {
     template,
+    controllerAs: name,
+    controller: Chat
   })
   .config(function config($stateProvider) {
     'ngInject';
