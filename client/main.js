32cbac9f7c7a8e76f2edf17b62ddaca93b6fef81
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-02 15:20:43 +0700

    tambah module App

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
-import angular from 'angular';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
-import { Meteor } from 'meteor/meteor';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { name as App } from '../imports/ui/components/app/app';
+
+angular.module('chat', [
+  angularMeteor,
+  App
+]);
