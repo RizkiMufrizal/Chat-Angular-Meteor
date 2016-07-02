321f6050c5367234802b0fd2597cac2a0c9802ea
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-02 16:20:43 +0700

    tambah module chat

--- imports/ui/components/app/app.js
+++ imports/ui/components/app/app.js
@@ -4,19 +4,22 @@ import uiRouter from 'angular-ui-router';
 
 import template from './app.html';
 import { name as Home } from '../home/home';
+import { name as Chat } from '../chat/chat';
 
 const name = 'app';
 
 export default angular.module(name, [
   angularMeteor,
   uiRouter,
+  Chat,
   Home
 ])
   .component(name, {
     template
   })
-  .config(function config($urlRouterProvider) {
+  .config(function config($locationProvider, $urlRouterProvider) {
     'ngInject';
 
-    $urlRouterProvider.otherwise('/home');
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
   });
