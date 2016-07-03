be4437676a71343181909a70bbe7021e54e48c82
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-03 12:33:29 +0700

    tambah signup dan signin

---
+++ imports/ui/components/user/user.js
@@ -0,0 +1,61 @@
+import { Meteor } from 'meteor/meteor';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import template from './user.html';
+
+class User {
+  constructor($scope, $reactive, $state) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+    _state = $state;
+
+    this.inputUserSignIn = {};
+    this.inputUserSignUp = {};
+  }
+
+  register(user) {
+    Accounts.createUser(user, function(error) {
+      if (error) {
+        console.log(error);
+      }
+      this.inputUserSignUp = {};
+      alert('anda berhasil register, silahkan login');
+    });
+  }
+
+  loginUser(user) {
+    Meteor.loginWithPassword(user.username, user.password, function(error) {
+      if (typeof error !== 'undefined') {
+        alert(error.reason);
+      } else {
+        alert('berhasil login');
+        _state.go('chat');
+      }
+    });
+  }
+
+}
+
+const name = 'user';
+
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter
+])
+  .component(name, {
+    template,
+    controllerAs: name,
+    controller: User
+  })
+  .config(function config($stateProvider) {
+    'ngInject';
+
+    $stateProvider
+      .state('user', {
+        url: '/authentication',
+        template: '<user></user>'
+      });
+  });
