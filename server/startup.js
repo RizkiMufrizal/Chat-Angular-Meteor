96d9722e66545b91bd2f88fea1e65d826fbe7f45
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-03 14:35:25 +0700

    update startup

--- server/startup.js
+++ server/startup.js
@@ -2,5 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import { Chats } from '../imports/api/chats';
 
 Meteor.startup(() => {
+  Chats.remove({});
+  Meteor.users.remove({});
+});
 
+Meteor.users.allow({
+  insert(userId, party) {
+    return userId;
+  },
+  update(userId, party, fields, modifier) {
+    return userId;
+  }
+});
+
+Chats.allow({
+  insert(userId, chat) {
+    return userId && chat.userId === userId;
+  }
 });
